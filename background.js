9a0ee3fcfbedc22ce10ebed1b9f7ab26df54a441
kevinwong197 <kevinwong1997@hotmail.com>
2018-07-31 10:50:57 -0400

    re2 is much easier than i thought

--- background.js
+++ background.js
@@ -1,11 +1,8 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'steamcommunity.com'},
+        pageUrl: {urlMatches: '^(https?:\/\/)?(www\.)?steamcommunity\.com\/(profiles|id)\/'},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
